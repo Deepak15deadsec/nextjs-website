d131ffd2b0b69b733f5d81dee5ae4d236b2c5711
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-03-13 21:49:01 +0530

    country code

---
+++ src/components/dropdowns/CountryDropdown.tsx
@@ -0,0 +1,60 @@
+
+
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+
+
+
+export default function CountryCode(props:any) {
+
+
+    return (
+        <div className=" w-92 h-full">
+            <Listbox value={props?.country} onChange={props?.setCountry}>
+                <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full h-full cursor-default bg-black rounded-l-[3rem] border-black opacity-80 bg-transparent py-2 pl-3 pr-10 text-left shadow-md focus:outline-none text-2xl">
+                        <span className="block truncate">{props.country.dial_code}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+
+                        </span>
+                    </Listbox.Button>
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {props?.countries.map((country:any, index:number) => (
+                                <Listbox.Option
+                                    key={index}
+                                    className={({ active }) =>
+                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                        }`
+                                    }
+                                    value={country}
+                                >
+                                    {({ selected }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                    }`}
+                                            >
+                                                {country.dial_code}
+                                            </span>
+                                            
+                                        </>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    )
+}
+
