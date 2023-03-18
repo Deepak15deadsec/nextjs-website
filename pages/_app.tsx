85fb6baad22288b881f5e568fca6f0c4c21410ca
onwoakash <akash.kumar@onwo.trade>
2023-03-18 12:06:45 +0530

    query resolving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { Open_Sans } from '@next/font/google'
 import Header from "../src/components/header"
 import Footer from "../src/components/footer"
-
+import { useRouter } from 'next/router'
 
 const open_sans = Open_Sans({
   subsets: ['latin'],
@@ -11,6 +11,14 @@ const open_sans = Open_Sans({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const { asPath, push, pathname } = useRouter()
+  if (asPath.split('?')[0] != pathname.split('?')[0] && !pathname.includes('[')) {
+    // Work around for next export breaking SPA routing on first hit
+    console.log('Browser route ' + asPath + ' did not match nextjs router route ' + pathname)
+    push(asPath)
+    return <div>Loading...</div>
+  }
   return (
     <main className={`${open_sans.variable} font-sans`}>
       <Header />
