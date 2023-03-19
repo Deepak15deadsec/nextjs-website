97a6c5fa3975db66921c2fb50142be41bf4d3d5b
onwoakash <akash.kumar@onwo.trade>
2023-03-19 20:43:33 +0530

    google url fixed

--- src/google/index.ts
+++ src/google/index.ts
@@ -1,16 +1,18 @@
 import useStore from "../../zustand/useStore";
 
 export const useGetGoogleOAuthURL = () => {
-  const {referrer, phone} = useStore((state) => state)
+  const { referrer, phone } = useStore((state) => state)
   const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`;
 
-  const options= {
+  const type = referrer === "" ? "general" : "referral"
+
+  const options = {
     redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URL || '',
     client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
     access_type: "offline",
     response_type: "code",
     prompt: "consent",
-    state: JSON.stringify({"referrer": referrer, "phone": phone, type: "referral"}),
+    state: JSON.stringify({ "referrer": referrer, "phone": phone, type: type }),
     scope: [
       "https://www.googleapis.com/auth/userinfo.profile",
       "https://www.googleapis.com/auth/userinfo.email",
